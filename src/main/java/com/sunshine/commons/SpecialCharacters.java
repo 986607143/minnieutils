d21c6717de130119160cc20b7a223c90a12df838
Sunshineuun <qiushengming@aliyun.com>
2017-08-24 18:19:24 +0800

    特殊字符定义
    特殊字符定义

---
+++ src/main/java/com/sunshine/commons/SpecialCharacters.java
@@ -0,0 +1,17 @@
+package com.sunshine.commons;
+
+/**
+ * <p>定义特殊字符的常量</p>
+ * <p>说明: </p>
+ * <p>备注: </p>
+ *
+ * @version 1.0
+ * @author 公司名 : 上海金仕达卫宁软件科技有限公司（Shanghai KingStar WinningSoft LTD.） <br />
+ * 变更履历 <br />
+ *
+ */
+public final class SpecialCharacters {
+
+/**该字符为0宽度啊的空白字符，unicode编码为[\u200b]*/
+public static final String ZERO_WIDTH_SPACE = "​";
+}
