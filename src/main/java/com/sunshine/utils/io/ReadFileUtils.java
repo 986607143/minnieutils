8654e2eabb849b1acabb991ae7ffabe02e487ae4
Sunshineuun <qiushengming@aliyun.com>
2017-08-24 18:23:21 +0800

    读写文件工具类

---
+++ src/main/java/com/sunshine/utils/io/ReadFileUtils.java
@@ -0,0 +1,62 @@
+package com.sunshine.utils.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class ReadFileUtils {
+    private final static String encoding =
+        System.getProperties().getProperty("file.encoding");
+
+    public static byte[] readByteFile(String fileName) throws IOException {
+        return readByteFile(new File(fileName));
+    }
+
+    public static byte[] readByteFile(File file) throws IOException {
+        byte[] result = null;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        FileInputStream input = new FileInputStream(file);
+        int count = 0;
+        byte[] b = new byte[1024];
+
+        try {
+            while (input.available() > 0) {
+                count = input.read(b);
+                if (count != -1) {
+                    out.write(b, 0, count);
+                }
+            }
+
+            result = out.toByteArray();
+        } finally {
+            input.close();
+            out.close();
+        }
+
+        return result;
+    }
+
+    public static String readCharFile(String fileName) throws IOException {
+        return readCharFile(new File(fileName));
+    }
+
+    public static String readCharFile(String fileName, String encoding)
+        throws UnsupportedEncodingException, IOException {
+        return readCharFile(new File(fileName), encoding);
+    }
+
+    public static String readCharFile(File file)
+        throws UnsupportedEncodingException, IOException {
+        return readCharFile(file, encoding);
+    }
+
+    public static String readCharFile(File file, String encoding)
+        throws UnsupportedEncodingException, IOException {
+        return new String(readByteFile(file), encoding);
+    }
+}
+
+/* Copyright (C) 2016, 上海金仕达卫宁软件科技有限公司 Project, All Rights Reserved. */
---
+++ src/main/java/com/sunshine/utils/io/WriteFileUtils.java
@@ -0,0 +1,121 @@
+package com.sunshine.utils.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+
+public class WriteFileUtils {
+    private static String encoding = System.getProperties().getProperty(
+            "file.encoding");
+
+
+    public WriteFileUtils() {
+        super();
+    }
+
+
+    public static void writeFile(String fileName, String content)
+        throws UnsupportedEncodingException, IOException {
+        writeFile(fileName, content, encoding);
+    }
+
+
+    public static void writeFile(String fileName, String content,
+        String encoding) throws UnsupportedEncodingException, IOException {
+        writeFile(fileName, content.getBytes(encoding));
+    }
+
+
+    public static void writeFile(String fileName, boolean append, String content)
+        throws UnsupportedEncodingException, IOException {
+        writeFile(fileName, append, content, encoding);
+    }
+
+
+    public static void writeFile(String fileName, boolean append,
+        String content, String encoding)
+        throws UnsupportedEncodingException, IOException {
+        writeFile(fileName, append, content.getBytes(encoding));
+    }
+
+
+    public static void writeFile(String fileName, byte[] bs) throws IOException {
+        writeFile(fileName, false, bs);
+    }
+
+
+    public static void writeFile(String fileName, boolean append, byte[] bs)
+        throws IOException {
+        writeFile(new FileOutputStream(fileName, append), bs);
+    }
+
+
+    public static void writeFile(OutputStream out, String content)
+        throws UnsupportedEncodingException, IOException {
+        writeFile(out, content, encoding);
+    }
+
+
+    public static void writeFile(OutputStream out, String content,
+        String encoding) throws UnsupportedEncodingException, IOException {
+        writeFile(out, content.getBytes(encoding));
+    }
+
+
+    public static void writeFile(OutputStream out, byte[] bs)
+        throws IOException {
+        try {
+            out.write(bs);
+            out.flush();
+        }
+        finally {
+            out.close();
+        }
+    }
+
+
+    public static void writeFile(Writer out, String content)
+        throws UnsupportedEncodingException, IOException {
+        writeFile(out, content, encoding);
+    }
+
+
+    public static void writeFile(Writer out, String content, String encoding)
+        throws UnsupportedEncodingException, IOException {
+        writeFile(out,
+                new String(content.getBytes(encoding), encoding).toCharArray());
+    }
+
+
+    public static void writeFile(Writer out, char[] cs) throws IOException {
+        try {
+            out.write(cs);
+            out.flush();
+        }
+        finally {
+            out.close();
+        }
+    }
+
+
+    public static void main(String args[]) {
+        String temp = "中文测试";
+        try {
+            WriteFileUtils.writeFile("d:\\a.txt", temp);
+//            WriteFileUtils.writeFile("d:\\b.txt", temp, "UTF-8");
+            WriteFileUtils.writeFile("d:\\a.txt", temp);
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+/* Copyright (C) 2016, 上海金仕达卫宁软件科技有限公司 Project, All Rights Reserved. */
